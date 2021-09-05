5959c6f70b5c2e223370597f272e4933cbe3058c
Rodrigo Merce de Andrade Silva <RMAV@santosbrasil.com.br>
2021-09-05 04:35:29 -0300

    Aula 03

--- main.js
+++ main.js
@@ -31,3 +31,30 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+
+/*Testimonials carousel slider swiper*/
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination:{
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/*SCROLLREVEAL - MOSTRAR ELEMENTOS QUANDO DER SCROLL NA PAG*/
+
+const scrollReveal = ScrollReveal({
+  origin:'top',
+  distance:'30px',
+  duration:700,
+  reset: true
+})
+
+scrollReveal.reveal(`#home .image, #home .text,
+#about .image, #about .text,
+#services header, #services card,
+#testimonials header, #testimonials .testimonials,
+#contact .text, #contact .links`, { interval:100 })
